160ff44c65f9d4416fc4ff39cd44d7d22b37402e
ONGUN, Dila <dilabatman@hotmail.com>
2023-04-07 10:45:14 +0200

    Implement character information section

--- src/modules/CharacterDetail/index.tsx
+++ src/modules/CharacterDetail/index.tsx
@@ -28,7 +28,7 @@ const CharacterDetail: React.FunctionComponent<CharacterDetailProps> = (props) =
 
     const isError = !selectedCharacterPending && selectedCharacterError;
     const isNoContent = !selectedCharacterPending && !selectedCharacterError && isEmpty(selectedCharacter);
-    
+
     useEffect(() => {
         const params = new URLSearchParams(location.search);
         const id = params.get("id") || "";
@@ -41,7 +41,34 @@ const CharacterDetail: React.FunctionComponent<CharacterDetailProps> = (props) =
         {selectedCharacterPending && <Loading />}
         {isError && <Error size={ErrorSize.lg} message="There is an error!" />}
         {isNoContent && <NoContent message="No data found :(" />}
+        {!isEmpty(selectedCharacter) &&
+            <>
+                <div className="character-detail__section">
+                    {selectedCharacter.name.length !== 0 && <h2>{selectedCharacter.name}</h2>}
+                    <div className="character-detail__information-wrapper">
+                        {selectedCharacter.image && <div className="character-detail__image">
+                            <img src={selectedCharacter.image} />
+                        </div>}
+                        <div className="character-detail__information-details">
+                            <CharacterInformationItem title="Status" informationValue={selectedCharacter.status} />
+                            <CharacterInformationItem title="Species" informationValue={selectedCharacter.species} />
+                            <CharacterInformationItem title="Gender" informationValue={selectedCharacter.gender} />
+                        </div>
+                    </div>
+                </div>
+                <div className="character-detail__section">Origin and location information</div>
+                <div className="character-detail__section">Name of the chapters</div>
+            </>}
     </>;
+}
+
+const CharacterInformationItem = (props: { title: string, informationValue: string }) => {
+    const { title, informationValue } = props;
+
+    return <>
+        <span className="character-detail__information-title">{title}</span>
+        <span>{informationValue}</span>
+    </>
 }
 
 const mapStateToProps = (state: AppState) => {
